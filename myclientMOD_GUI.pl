aae0f1f4ff4c15647d5c4d73412b20fc5cbe2590
Tarzan9192 <joshwhtny@yahoo.com>
2017-03-02 02:00:01 -0800

    Almost finished, error upon client exit

--- myclientMOD_GUI.pl
+++ myclientMOD_GUI.pl
@@ -1,67 +1,89 @@
 #!/usr/bin/perl
-
+use strict;
+use warnings;
 use IO::Socket::INET;
 use threads;
 use Tk;
 
 $| = 1;
 
-my ($socket,$client_socket,$data,$userName);
+my ($socket,$client_socket,$data,$userName, $message);
 
 
 # GUI area below
+# Main Window
+$data = "";
+$userName = "";
+$message = "";
+
 # Main Window
 my $mw = MainWindow->new(-height => 600, -width => 600);
 
 #Frame for the chatbox display
 my $textFrame = $mw -> Frame(-width => 40, -height => 10);
-$textFrame -> grid(-row => 1, -column => 1, -columnspan => 2, -sticky => "nsew");
+
+# $textFrame -> pack;
 
 #Actual textbox for the chat display                              
-my $textBox = $textFrame -> Text(-background => white);
-$textBox -> grid(-row => 1, -column => 1, -columnspan => 2, -sticky => "nsew");
+# my $textBox = $textFrame -> Text(-background => white);
+# $textBox -> grid(-row => 1, -column => 1, -columnspan => 2, -sticky => "nsew");
+my $textBox = $textFrame-> Scrolled('Text', 
+                                    -scrollbars => 'ose',                                    
+                                    -background => "white",
+                                     );
 
-my $srl_y = $textFrame -> Scrollbar(-orient=>'v', -command=>[yview => $textBox]);
-my $srl_x = $textFrame -> Scrollbar(-orient=>'h',-command=>[xview => $textBox]);
-$textBox -> configure(-yscrollcommand=>['set', $srl_y], 
-		-xscrollcommand=>['set',$srl_x]);
+my $button = $mw -> Button(-text => "Send", -command => \&broadcast);
 
-$srl_y -> grid(-row => 1, -column => 3, -sticky => "ns");
-$srl_x -> grid(-row => 2, -column => 1, -sticky => "ew", -columnspan => 2);
-$textBox -> grid(-sticky => "nsew");            
+my $entry = $mw -> Entry(-background => "white",
+                              -width => 40,
+                              -textvariable => \$message);
+# $eJoshntry -> focus;                              
+
+# my $srl_y = $textFrame -> Scrollbar(-orient=>'v', -command=>[yview => $textBox]);
+# my $srl_x = $textFrame -> Scrollbar(-orient=>'h',-command=>[xview => $textBox]);
+# $textBox -> configure(-yscrollcommand=>['set', $srl_y], 
+# 		-xscrollcommand=>['set',$srl_x]);
+
+# $srl_y -> grid(-row => 1, -column => 3, -sticky => "ns");
+# $srl_x -> grid(-row => 2, -column => 1, -sticky => "ew", -columnspan => 2);
+# $textBox -> grid(-sticky => "nsew");            
             
-
-# my $label = $mw -> Label(-text=>"Hello World");
-# $label -> grid(-row => 1, -column => 1);
-my $button = $mw -> Button(-text => "Send", 
-      -command => \&btnSend);
-$button -> grid(-row => 2, -column => 2, -sticky => "ew");
-
-my $entry = $mw -> Entry(-background => white,
-                              -width => 40);
-$entry -> grid(-row => 2, -column => 1, -sticky => "ew");
-
 tie *STDOUT, 'Tk::Text', $textBox;
 # tie *STDIN, 'Tk::Entry', $entry;
+$entry -> bind('<Return>', \&broadcast);
 
-# $mw->configure(-width=> 40, -height=> 250);
+$entry -> focus;
+
+
+
+# Geometry Management
+$textFrame -> grid(-row => 1, -column => 1, -columnspan => 2, -sticky => "nsew");
+$textBox -> grid(-row => 1, -column => 1, -sticky => "nsew");
+$button -> grid(-row => 2, -column => 2, -sticky => "ew");
+$entry -> grid(-row => 2, -column => 1, -sticky => "ew");
 $mw -> gridColumnconfigure(1, -weight => 1);
 $mw -> gridColumnconfigure(3, -weight => 1);
 $mw -> gridRowconfigure(1, -weight => 1);
 $mw -> resizable(0,0);
-MainLoop;
+ 
 
-sub btnSend {
-      my $string = $entry -> get();
-      print $string;
-      print "\n";
-      $entry -> delete(0, 'end'); 
+
+
+sub broadcast {
+      my $string = $message;
+      if($userName eq ""){
+            $userName = $message;
+            print $userName;                                  
+      } 
+      print "\n";       
+      if ($message eq "e"){$socket->send("e");exit();}
+      else{$socket->send($message);}   
+      $entry -> delete(0, 'end');  
 }
 
 # ---End GUI construction---
 
-print "LOGIN\nUsername: ";
-chomp($userName = <STDIN>);
+
 
 $socket = new IO::Socket::INET (
 PeerHost => '127.0.0.1',
@@ -69,30 +91,33 @@ PeerPort => '22222',
 Proto => 'tcp',
 ) or die "ERROR in Socket Creation : $!\n";
 
-print "Connected to Chat Server\n";
-$socket->send($userName);
+# print "Connected to Chat Server\n";
+print "LOGIN\nUsername: ";
+# $socket->send($userName);
 
 sub reader {
   while(1){
-    my $data = <$socket>;
+    $data = <$socket>;
     chomp($data);
     print "$data\n";
   }
 }
 
+
+
 my $thr = threads->create(\&reader)->detach();
 
-sub userInput{
-    if ($message eq "e"){$socket->send("e");exit();}
-    else{$socket->send($message);}
-}
+# sub userInput{
+#     if ($message eq "e"){$socket->send("e");exit();}
+#     else{$socket->send($message);}
+# }
 
-while(1)
-{
-    chomp($message = <STDIN>);
-    userInput();
-}
-
+# while(1)
+# {
+#     # chomp($message = <STDIN>);
+#     userInput();
+# }
+MainLoop;
 
 
 $socket->close();
