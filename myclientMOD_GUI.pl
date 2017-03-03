55844a6cdfd3d9566d3894d79cf2247ca0c1ff43
Tarzan9192 <joshwhtny@yahoo.com>
2017-03-03 01:17:08 -0800

    Fixed lagging, added comments

--- myclientMOD_GUI.pl
+++ myclientMOD_GUI.pl
@@ -9,6 +9,11 @@ $| = 1;
 
 my ($socket,$client_socket,$data,$userName, $message);
 
+$socket = new IO::Socket::INET (
+PeerHost => '127.0.0.1',
+PeerPort => '22222',
+Proto => 'tcp',
+) or die "ERROR in Socket Creation : $!\n";
 
 # GUI area below
 # Main Window
@@ -19,22 +24,36 @@ $message = "";
 # Main Window
 my $mw = MainWindow->new(-height => 600, -width => 600);
 
+# Listen for message from server using fileevent method
+$mw ->fileevent($socket, readable => sub {
+    my $line = <$socket>;
+    unless (defined $line) {
+      $mw->fileevent($socket => readable => '');
+      return;
+    }      
+    chomp($line);
+    print "$line\n";
+});
+
 
 
 #Frame for the chatbox display
 my $textFrame = $mw -> Frame(-width => 40, -height => 10);
 
-my $textBox = $textFrame-> Scrolled('Text', 
+my $textBox = $textFrame-> Scrolled('ROText', 
                                     -scrollbars => 'ose',                                    
-                                    -background => "white",
+                                    -background => "white",                                                                    
                                      );
 
 my $button = $mw -> Button(-text => "Send", -command => \&broadcast);
 
+# Here we define the Entry widget and use the -textvariable
+# option to assign the input string to the $message variable.
 my $entry = $mw -> Entry(-background => "white",
                               -width => 40,
                               -textvariable => \$message);          
-            
+
+# This line ties the standard output stream to the $textbox
 tie *STDOUT, 'Tk::Text', $textBox;
 
 # To send input simply by pressing 'Enter'
@@ -55,19 +74,19 @@ $mw -> gridColumnconfigure(3, -weight => 1);
 $mw -> gridRowconfigure(1, -weight => 1);
 $mw -> resizable(0,0);
  
+# ---End GUI construction---
 
-
-# Sub to broadcast to server
+# Method to broadcast to server
 sub broadcast {
       my $string = $message;
 
       # If user has not entered anything yet, 
       # set username to input
       if($userName eq ""){
-            $userName = $message;
-            print $userName;                                  
-      } 
-      print "\n"; 
+            $userName = $message;            
+            print $userName; 
+            print "\n"; 
+      }       
 
       if ($message eq "e"){$socket->send("e");exit();}
       else{$socket->send($message);} 
@@ -76,32 +95,7 @@ sub broadcast {
       $entry -> delete(0, 'end');  
 }
 
-# ---End GUI construction---
-
-
-
-$socket = new IO::Socket::INET (
-PeerHost => '127.0.0.1',
-PeerPort => '22222',
-Proto => 'tcp',
-) or die "ERROR in Socket Creation : $!\n";
-
-
 print "LOGIN\nUsername: ";
-# $socket->send($userName);
-
-# Sub to read output from server
-sub reader {
-  while(1){
-    $data = <$socket>;
-    chomp($data);
-    print "$data\n";
-  }
-}
-
-
-# spawn thread for the server output reader
-my $thr = threads->create(\&reader)->detach();
 
 
 # Send exit call to server
--- myserverMOD.pl
+++ myserverMOD.pl
@@ -12,6 +12,7 @@ my ($name,$tName,$message,$iter);
 our @clients : shared;
 @clients = ();
 
+# Create socket here
 my $socket = new IO::Socket::INET (
 LocalHost => '127.0.0.1',
 LocalPort => '22222',
@@ -22,28 +23,54 @@ Reuse => 1
 
 print "SERVER Waiting for client connection on port 22222\n";
 
-
+# Method to initialize a new client/server connection
 sub process {
     my $data;
+
+    # This line maps the array elements passed form our
+    # thread 'create()' method to local variables.
     my ($lname, $lclient, $lfileno) = @_;
+
     print "$lname Connected.\n";
     print $lclient "Welcome to server\n";
+
+    # Here we write a conneciton method to each client
+    # in the client list.
     foreach my $fn (@clients){
         open my $fh, ">&=$fn" or warn $! and die;
         print $fh "$lname Connected.\n";
     }
     while(1){
+        # Listen for message from clients
         $lclient->recv($message, 1024);
+
+        # Indicates the client has has disconnected
         if($message eq "e"){
+
+            # This line uses Perl's grep() method to remove the 
+            # file handle corresponding to $lfileno from the 
+            # @clients list. It returns a list of the elements for
+            # which the code block evaluates to true. Inside the block
+            # the $_ variable corresponds to the current list element,
+            # while the '!~' operator attempts to match the element with
+            # the variable on the right and returns a boolean 'false' if
+            # they DO MATCH, and 'true' if they DO NOT MATCH.
             @clients = grep {$_ !~ $lfileno} @clients;
+
             print "$lname disconnected.\n";
+
+            # Print a message to all other clients that $lfileno has
+            # disconnected.
             foreach my $fn (@clients){
                 open my $fh, ">&=$fn" or warn $! and die;
                 print $fh "$lname disconnected.\n";
             }
-            #splice(@clients,$iter,1);
+            
+            # Kill this particular thread
             threads->exit();
         }
+
+        # Echo clients message to all other clients in the @clients list
         else{
             foreach my $fn (@clients){
                 open my $fh, ">&=$fn" or warn $! and die;
@@ -54,12 +81,27 @@ sub process {
     close($lclient);
 }
 
+# Main server loop
 while(1)
 {
+    # wait for client to connect
     my $client_socket = $socket->accept();
+
+    # Get client name from socket outputstream
     $client_socket->recv($name, 1024);
+
+    # Perl doesn't allow us to share an array of client sockets,
+    # so we will create an array of references to those sockets instead
+    # to share among the threads created.
     my $fileno = fileno $client_socket;
     push (@clients, $fileno);
+
+    # Here we pass the 'process' method by reference to the thread 'create()' method.
+    # The other arguments will be passed as arguments to the process funciton
+    # when when it is called.
+
+    # '-> detach()' call  prevents the main program from blocking while Waiting
+    # for the newly created thread to complete its execution.
     my $thr = threads->create(\&process, $name, $client_socket, $fileno)->detach();
 }
 $socket->close();
